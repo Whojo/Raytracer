393375b6743d77f758c8b7a5da1027a1a2cf80e2
Guillaume THOMAS <guillaume1.thomas@epita.fr>
2022-02-27 23:50:25 +0100

    Improve specular

--- color.cpp
+++ color.cpp
@@ -1,11 +1,13 @@
 #include "color.hpp"
 
+#include <algorithm>
+
 
 Color Color::operator*(const double alpha) const // TODO: pass to HSL for cleaner color convertion
 {
-    return Color{static_cast<uint8_t>(RED   * alpha),
-                 static_cast<uint8_t>(GREEN * alpha),
-                 static_cast<uint8_t>(BLUE  * alpha)};
+    return Color{static_cast<uint8_t>(std::min(RED * alpha, 255.0)),
+                 static_cast<uint8_t>(std::min(GREEN * alpha, 255.0)),
+                 static_cast<uint8_t>(std::min(BLUE  * alpha, 255.0))};
 }
 
 std::ostream& operator<<(std::ostream &out , const Color &color)
--- color.hpp
+++ color.hpp
@@ -9,9 +9,9 @@ public:
     Color operator*(const double alpha) const;
 
 public:
-    uint8_t RED;
-    uint8_t GREEN;
-    uint8_t BLUE;
+    uint16_t RED;
+    uint16_t GREEN;
+    uint16_t BLUE;
 };
 
 std::ostream& operator<<(std::ostream &out , const Color &color);
