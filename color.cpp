f38b42ff675bfc84e21825bf54ee9c51e6b86408
Guillaume THOMAS <guillaume1.thomas@epita.fr>
2022-02-27 16:17:30 +0100

    Add intensity computing on color

--- color.cpp
+++ color.cpp
@@ -1,6 +1,13 @@
 #include "color.hpp"
 
 
+Color Color::operator*(const double alpha) const // TODO: pass to HSL for cleaner color convertion
+{
+    return Color{static_cast<uint8_t>(RED   * alpha),
+                 static_cast<uint8_t>(GREEN * alpha),
+                 static_cast<uint8_t>(BLUE  * alpha)};
+}
+
 std::ostream& operator<<(std::ostream &out , const Color &color)
 {
     return out << "("  << (short) color.RED
--- color.hpp
+++ color.hpp
@@ -6,6 +6,9 @@
 class Color
 {
 public:
+    Color operator*(const double alpha) const;
+
+public:
     uint8_t RED;
     uint8_t GREEN;
     uint8_t BLUE;
