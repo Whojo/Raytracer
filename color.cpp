cb9fc54dc38bcec73a0f7368b5062cd3a35a0f11
Guillaume THOMAS <guillaume1.thomas@epita.fr>
2022-02-28 09:18:17 +0100

    fixup! Add reflection

--- color.cpp
+++ color.cpp
@@ -15,14 +15,14 @@ static uint16_t add_color_canal(const uint16_t i1, const uint16_t i2)
     auto max = std::max(i1, i2);
     auto min = std::min(i1, i2);
 
-    return max + min * (1 - max) / 2;
+    return max + (min / 255.0) * (255 - max) / 2;
 }
 
-Color Color::operator+(const Color c) const
+Color Color::operator+(const Color &c) const
 {
     return Color{ add_color_canal(RED, c.RED),
                   add_color_canal(GREEN, c.GREEN),
-                  add_color_canal(BLUE, c.BLUE) };
+                  add_color_canal(BLUE, c.BLUE)};
 }
 
 std::ostream& operator<<(std::ostream &out , const Color &color)
